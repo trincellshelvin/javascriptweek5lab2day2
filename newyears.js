35d019dc1c83d9a27585ad5c62b7ee2c5e1b43a0
trincellshelvin <trincellshelvin@gmail.com>
2024-06-24 16:45:32 +0000

    update

--- newyears.js
+++ newyears.js
@@ -1,1 +1,10 @@
-
+funtion balldropCreate(){
+  let name;
+  let guest;
+  name = prompt("What is your name?");
+  guest = prompt("Whom would you like to invite?");
+  newyearsHeader.innerHTML = `You're Invited....`;
+  newyearsParagraph.innerHTML = `Hi ${guest}! ${name} would like to invite you to join them to watch the ball drop into the New Year. `;
+  
+}
+  newyearsButton.onclick = balldropCreate
